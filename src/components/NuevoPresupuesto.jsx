18158c9c94bba5b0b92a4115febe18bfc67bc19a
mbarrios <martin_18_28@hotmail.es>
2023-08-28 12:34:33 -0300

    Primer Commit

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,17 @@
+
+
+const NuevoPresupuesto = () => {
+  return (
+    <div className="contenedor-presupuesto contenedor sombra">
+      <form className="formulario">
+            <div className="campo">
+                <label >Definir presupuesto</label>
+                <input type="text"  className="nuevo-presupuesto" placeholder="Añade tu presupuesto"/>
+            </div>
+            <input type="submit" value="Añadir" />
+      </form>
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
